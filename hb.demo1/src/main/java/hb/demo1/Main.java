481c40a4664c050c53ead6dcf1b6538001bcbae2
ashish-panicker <ashish.s.panicker@gmail.com>
2020-01-03 09:42:33 +0530

    hb crud

--- hb.demo1/src/main/java/hb/demo1/Main.java
+++ hb.demo1/src/main/java/hb/demo1/Main.java
@@ -5,9 +5,13 @@ import org.hibernate.boot.Metadata;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.query.Query;
+
 import hb.demo1.model.Book;
 
 import java.util.List;
+
+import javax.persistence.TypedQuery;
 
 import org.hibernate.Session;
 import org.hibernate.Transaction;
@@ -20,34 +24,27 @@ public class Main {
 	public static void main(String[] args) {
 
 		//populate();
+			
+		delete(105);
 		
-		List<Book> books = read();
-		
+		System.out.println("After updating..");
+		List<Book> books  =  read();
 		for (Book book : books) {
 			System.out.println(book);
 		}
-		
 		shutdown();
 
 	}
 	
 	public static void populate() {
 		create(101, "Halo Envoy", "Eric Nylund", 999);
-		create(102, "Halo Envoy", "Eric Nylund", 998);
+		create(102, "Halo First Strike", "Eric Nylund", 998);
 		create(102, "Halo Smoke And Shadow", "Kelly Gay", 997);
 		create(103, "Halo Renegades", "Kelly Gay", 996);
 		create(104, "Halo Last Light", "Troy Dennigs", 995);
 		create(105, "Halo Silent Storm", "Troy Dennigs", 994);
 	}
 	
-	public static List<Book> read(){
-		List<Book> books = null;
-		try (Session session = getSessionFactory().openSession()) {
-			books = session.createQuery("select b from Book b", Book.class).list();
-		}
-		return books;
-	}
-
 	public static void create(int id, String title, String author, int isbn) {
 		Transaction transaction = null;
 		try (Session session = getSessionFactory().openSession()) {
@@ -57,6 +54,45 @@ public class Main {
 			transaction.commit();
 		}
 	}
+	
+	public static List<Book> read(){
+		List<Book> books = null;
+		try (Session session = getSessionFactory().openSession()) {
+			Query<Book> q =  session.createQuery("select b from Book b", Book.class);
+			books = q.getResultList();
+		}
+		return books;
+	}
+	
+	public static void update(int id) {
+		try (Session session = getSessionFactory().openSession()) {
+			Book b = session.get(Book.class, id);
+			b.setAuthor("Troy Dennig");
+			session.getTransaction().begin();
+			session.update(b);
+			session.getTransaction().commit();
+			System.out.println(session.getTransaction().getStatus().toString());
+		}
+	}
+	
+	public static void delete(int id) {
+		try (Session session = getSessionFactory().openSession()) {
+			Transaction t  = null;
+			try {
+				Book b = session.get(Book.class, id);
+				t = session.getTransaction();
+				t.begin();
+				session.delete(b);
+				t.commit();
+			}catch(Exception ex) {
+				t.rollback();
+				ex.printStackTrace();
+			}
+			
+		}
+	}
+	
+
 
 	public static SessionFactory getSessionFactory() {
 		if (sessionFactory == null) {
